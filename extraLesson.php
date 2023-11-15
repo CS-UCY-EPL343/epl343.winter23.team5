bbece44b4b83204eca1d296f3cf5f1ac6d1cadb4
Christos Iosif <ciosif01@ucy.ac.cy>
2023-11-15 23:14:55 +0200

    Added database connection and performed queries

--- extraLesson.php
+++ extraLesson.php
@@ -4,12 +4,13 @@
 if (isset($_POST["class"]) && isset($_POST["date"]) && isset($_POST["timeFrom"]) && isset($_POST["timeTo"])) {
     session_start();
     include 'user.php';
+    include 'DatabaseHandler.php';
     $serialized = $_SESSION['user'];
     $retrievedUser = unserialize($serialized);
 
     function checkData($data) {
         if(empty($data)){
-            header("Location: index.php");
+            header("Location: temp.php");
             exit();
         }
         $data = trim($data);
@@ -19,20 +20,27 @@ if (isset($_POST["class"]) && isset($_POST["date"]) && isset($_POST["timeFrom"])
     $date = checkData($_POST["date"]);
     $timeFrom = checkData($_POST["timeFrom"]);
     $timeTo = checkData($_POST["timeTo"]);
+    $formattedTimeFrom = date('Hi', strtotime($timeFrom));
+    $formattedTimeTo = date('Hi', strtotime($timeTo));
     
     
-    if(strlen($timeFrom)!=4 || strlen($timeTo)!= 4 || $class<=0){
-        header("Location: index.php");
-        exit();
-    }
 
-
-
-    $timeConc = $timeFrom . $timeTo;
+    $timeConc = $formattedTimeFrom . " - " . $formattedTimeTo;
     #$sql = "INSERT INTO ExtraLesson (ELdate, ELtime, CID)
     #VALUES ($date, $timeConc, $class)";
+    
+    $classes = $_SESSION['classes'];
 
-
+    if ($class < 1 || $class > count($classes)) {
+        header("Location: temp.php");
+        exit();
+    }
+    /*
+    $database = new Dbh();
+    $sql = "CALL insert_extra_lesson(:ELDate, :ELTime, :CID)";
+    $params = [':ELDate' => $date, ':ELTime' => $timeConc, ':CID' => $classes[$class -1]['CID']];
+    $database->executeQuery($sql, $params);
+    */
     echo "Hello " . $retrievedUser->getUsername() . "<br>";
     echo "Submission successful!<br>";
     echo "Class: ". $class. "<br>Date: "  .$date.  "<br>Time: " . $timeConc;
