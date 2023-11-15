a9bced62a724ddcc929794e97fb6565512f21ac0
JuliosFotiou <jfotio01@ucy.ac.cy>
2023-11-15 14:42:21 +0200

    createTable.sql Changed

--- createTables.sql
+++ createTables.sql
@@ -3,11 +3,11 @@ CREATE TABLE Users (
     Fname char(36) not null,
     Lname char(36) not null,
     username char(36) not null,
-    password char(72) not null,
-    Type tinyint not null,
+    Upassword char(72) not null,
+    UType tinyint not null,
     isEnrolled bit not null,
     Phone int not null,
-    CONSTRAINT CHK_UTYPE CHECK (Type>=0 AND Type<=2),
+    CONSTRAINT CHK_UTYPE CHECK (UType>=0 AND UType<=2),
     CONSTRAINT CHK_UENROLL CHECK (isEnrolled>=0 AND isEnrolled<=1),
     CONSTRAINT CHK_UPHONE CHECK  ((Phone >= 96000000 AND Phone <= 96999999)
                                     OR (Phone >= 99000000 AND Phone <= 99999999) 
@@ -18,19 +18,19 @@ CREATE TABLE Users (
 
 CREATE TABLE Class(
     CID tinyint not null AUTO_INCREMENT PRIMARY KEY,
-    Name char(1) not null,
+    CName char(1) not null,
     SchoolYear tinyint not null,
-    Number tinyint not null,
+    CNumber tinyint not null,
     AvailableSeats tinyint not null,
-    Days char(7) not null,
+    CDays char(7) not null,
     TimeForFirstDay char(8) not null,
     TimeForSecondDay char(8) not null,
     NextYears bit not null,
-     CONSTRAINT CHK_CLASSNAME CHECK (Name IN ('M','P','C')),
+     CONSTRAINT CHK_CLASSNAME CHECK (CName IN ('M','P','C')),
      CONSTRAINT CHK_CLASSSCHOOLY CHECK (SchoolYear>=1 AND SchoolYear<=6),
-     CONSTRAINT CHK_CLASSNUM CHECK (Number>=0 AND Number<=5),
+     CONSTRAINT CHK_CLASSNUM CHECK (CNumber>=0 AND CNumber<=5),
      CONSTRAINT CHK_CLASSAVLSEATS CHECK (AvailableSeats>=0 AND AvailableSeats<=10),
-     CONSTRAINT CHK_CLASSDAYS CHECK (Days REGEXP '^[01]{7}$'),
+     CONSTRAINT CHK_CLASSDAYS CHECK (CDays REGEXP '^[01]{7}$'),
      CONSTRAINT CHK_CLASSTIMEFIRSTD CHECK (
         SUBSTRING(TimeForFirstDay, 1, 1) BETWEEN '0' AND '2'
         AND SUBSTRING(TimeForFirstDay, 2, 1) BETWEEN '0' AND '9'
@@ -53,20 +53,19 @@ CREATE TABLE Class(
 );
 
 CREATE TABLE ExtraLesson(
-    Days char(7) not null,
-    Time char(8) not null,
+    ELDate date not null,
+    ELTime char(8) not null,
     CID tinyint not null,
     ELID smallint not null AUTO_INCREMENT PRIMARY KEY,
-    CONSTRAINT CHK_ELESSOND CHECK (Days REGEXP '^[01]{7}$'),
     CONSTRAINT CHK_ELESSONT CHECK (
-        SUBSTRING(Time, 1, 1) BETWEEN '0' AND '2'
-        AND SUBSTRING(Time, 2, 1) BETWEEN '0' AND '9'
-        AND SUBSTRING(Time, 3, 1) BETWEEN '0' AND '5'
-        AND SUBSTRING(Time, 4, 1) BETWEEN '0' AND '9'
-        AND SUBSTRING(Time, 5, 1) BETWEEN '0' AND '2'
-        AND SUBSTRING(Time, 6, 1) BETWEEN '0' AND '9'
-        AND SUBSTRING(Time, 7, 1) BETWEEN '0' AND '5'
-        AND SUBSTRING(Time, 8, 1) BETWEEN '0' AND '9'
+        SUBSTRING(ELTime, 1, 1) BETWEEN '0' AND '2'
+        AND SUBSTRING(ELTime, 2, 1) BETWEEN '0' AND '9'
+        AND SUBSTRING(ELTime, 3, 1) BETWEEN '0' AND '5'
+        AND SUBSTRING(ELTime, 4, 1) BETWEEN '0' AND '9'
+        AND SUBSTRING(ELTime, 5, 1) BETWEEN '0' AND '2'
+        AND SUBSTRING(ELTime, 6, 1) BETWEEN '0' AND '9'
+        AND SUBSTRING(ELTime, 7, 1) BETWEEN '0' AND '5'
+        AND SUBSTRING(ELTime, 8, 1) BETWEEN '0' AND '9'
     )
 );
 
