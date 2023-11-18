10bccd09c10c02066cd7a5fe0ada641be62c47dd
JuliosFotiou <jfotio01@ucy.ac.cy>
2023-11-18 13:03:05 +0200

    DROP Stored Procedure

---
+++ spDropProcedures.sql
@@ -0,0 +1,35 @@
+-- Drop get_user procedure
+DROP PROCEDURE IF EXISTS get_user;
+
+-- Drop find_user procedure
+DROP PROCEDURE IF EXISTS find_user;
+
+-- Drop insert_to_teaching procedure
+DROP PROCEDURE IF EXISTS insert_to_teaching;
+
+-- Drop insert_to_belongsto procedure
+DROP PROCEDURE IF EXISTS insert_to_belongsto;
+
+-- Drop add_user procedure
+DROP PROCEDURE IF EXISTS add_user;
+
+-- Drop find_teaching_classes procedure
+DROP PROCEDURE IF EXISTS find_teaching_classes;
+
+-- Drop insert_extra_lesson procedure
+DROP PROCEDURE IF EXISTS insert_extra_lesson;
+
+-- Drop fetch_teachers procedure
+DROP PROCEDURE IF EXISTS fetch_teachers;
+
+-- Drop add_class procedure
+DROP PROCEDURE IF EXISTS add_class;
+
+-- Drop update_class procedure
+DROP PROCEDURE IF EXISTS update_class;
+
+-- Drop get_all_classes procedure
+DROP PROCEDURE IF EXISTS get_all_classes;
+
+-- Drop delete_class procedure
+DROP PROCEDURE IF EXISTS delete_class;
