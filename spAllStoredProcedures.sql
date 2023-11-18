c1fcb08e1ee66bd50f04e0607ee96c147cf8df5a
JuliosFotiou <jfotio01@ucy.ac.cy>
2023-11-18 17:07:22 +0200

    ALL Stored Procedures

--- spAllStoredProcedures.sql
+++ spAllStoredProcedures.sql
@@ -251,3 +251,16 @@ END //
 
 DELIMITER ;
 
+
+-- CREATE PROCEDURE delete_user
+DELIMITER //
+
+CREATE PROCEDURE delete_user(IN p_username char(36))
+
+BEGIN
+    DELETE u  
+    FROM Users u 
+    WHERE u.username=p_username;
+END //
+
+DELIMITER ;
