2f943a4884ef62d65f8141a7da449e8991a09a30
JuliosFotiou <jfotio01@ucy.ac.cy>
2023-11-18 12:22:53 +0200

    Stored Procedure add_class

---
+++ spAddClass.sql
@@ -0,0 +1,35 @@
+DELIMITER //
+
+CREATE PROCEDURE add_class(
+    IN p_CName char(1),
+    IN p_SchoolYear tinyint,
+    IN p_CNumber tinyint,
+    IN p_AvailableSeats tinyint,
+    IN p_CDays char(7),
+    IN p_TimeForFirstDay char(8),
+    IN p_TimeForSecondDay char(8),
+    IN p_NextYears bit
+)
+BEGIN
+    INSERT INTO Class (
+        CName,
+        SchoolYear,
+        CNumber,
+        AvailableSeats,
+        CDays,
+        TimeForFirstDay,
+        TimeForSecondDay,
+        NextYears
+    ) VALUES (
+        p_CName,
+        p_SchoolYear,
+        p_CNumber,
+        p_AvailableSeats,
+        p_CDays,
+        p_TimeForFirstDay,
+        p_TimeForSecondDay,
+        p_NextYears
+    );
+END //
+
+DELIMITER ;
