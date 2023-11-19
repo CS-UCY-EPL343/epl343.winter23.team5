a25ea0e17ff9022ff44dd9d5a8f77567c847b2d8
JuliosFotiou <jfotio01@ucy.ac.cy>
2023-11-19 21:23:15 +0200

    Stored Procedure find_teaching_classes updated

--- spFindTeachingClasses.sql
+++ spFindTeachingClasses.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE find_teaching_classes(
     IN p_username char(36)
 )
 BEGIN
-SELECT c.CID, c.CName, c.SchoolYear, c.CNumber, c.CDays, c.TimeForFirstDay, c.TimeForSecondDay
+SELECT *
 FROM Class c
 INNER JOIN Teaching t ON t.CID=c.CID
 INNER JOIN Users u ON u.UserID=t.UserID
