864386278eba175089ade5fb67d4b7eb955b1e0e
JuliosFotiou <jfotio01@ucy.ac.cy>
2023-11-22 19:03:15 +0200

    Stored Procedure fetch_other_students Updated

--- spFetchOtherStudents.sql
+++ spFetchOtherStudents.sql
@@ -10,7 +10,7 @@ BEGIN
         SELECT u2.*
         FROM Users u2
         INNER JOIN BelongsTo b ON u2.UserID = b.UserID
-        INNER JOIN CLASS c ON b.CID = c.CID
+        INNER JOIN Class c ON b.CID = c.CID
         WHERE c.CID = p_CID AND u1.UserID = u2.UserID
     );
 END //
