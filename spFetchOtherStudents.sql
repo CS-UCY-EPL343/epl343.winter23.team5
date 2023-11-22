2bcc9d495d876e64ec87e822f0333357c3822294
JuliosFotiou <jfotio01@ucy.ac.cy>
2023-11-22 12:10:56 +0200

    Stored Procedure fetch_other_students

---
+++ spFetchOtherStudents.sql
@@ -0,0 +1,18 @@
+DELIMITER //
+
+CREATE PROCEDURE fetch_other_students(IN p_CID TINYINT)
+
+BEGIN
+    SELECT u1.*
+    FROM Users u1
+    WHERE u1.UType = 0
+    AND NOT EXISTS (
+        SELECT u2.*
+        FROM Users u2
+        INNER JOIN BelongsTo b ON u2.UserID = b.UserID
+        INNER JOIN CLASS c ON b.CID = c.CID
+        WHERE c.CID = p_CID AND u1.UserID = u2.UserID
+    );
+END //
+
+DELIMITER ;
