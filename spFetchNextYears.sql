1e9eadca8bce7bc4112a564088e6329e9b1ed9b6
JuliosFotiou <jfotio01@ucy.ac.cy>
2023-11-23 22:34:48 +0200

    Stored Procedure fetch_next_years Updated

--- spFetchNextYears.sql
+++ spFetchNextYears.sql
@@ -1,11 +1,11 @@
 DELIMITER //
 
-CREATE PROCEDURE fetch_next_years()
+CREATE PROCEDURE fetch_next_years(p_SchoolYear tinyint)
 
 BEGIN
     SELECT *
     FROM Class c
-    WHERE c.NextYears=1;
+    WHERE c.NextYears=1 AND c.SchoolYear=p_SchoolYear;
 END //
 
 DELIMITER ;
