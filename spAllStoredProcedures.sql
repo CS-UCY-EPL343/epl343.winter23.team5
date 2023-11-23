4f1e677fd798683e1db126def3ee96e4de91a7be
JuliosFotiou <jfotio01@ucy.ac.cy>
2023-11-23 20:48:35 +0200

    ALL Stored Procedures Updated

--- spAllStoredProcedures.sql
+++ spAllStoredProcedures.sql
@@ -362,3 +362,17 @@ BEGIN
 END //
 
 DELIMITER ;
+
+
+-- CREATE PROCEDURE fetch_next_years
+DELIMITER //
+
+CREATE PROCEDURE fetch_next_years()
+
+BEGIN
+    SELECT *
+    FROM Class c
+    WHERE c.NextYears=1;
+END //
+
+DELIMITER ;
